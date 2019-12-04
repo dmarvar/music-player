d8828badd11b9fa3605fd6a116127d023c6cbcee
Arturo Martinez <d.martinez@sisproconsultores.com>
2019-12-03 19:20:30 -0500

    Updates

---
+++ precache-manifest.e6f837b2d4b22dbad8226a11a11e2046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6813d3d92d268adadae9690051696b6",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "ee9a3153214860fef769",
+    "url": "/music-player/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dc12ca9ed364cc6db644",
+    "url": "/music-player/static/js/2.e748856e.chunk.js"
+  },
+  {
+    "revision": "ee9a3153214860fef769",
+    "url": "/music-player/static/js/main.961cd6e2.chunk.js"
+  },
+  {
+    "revision": "62c3d9ba1db39cba8e95",
+    "url": "/music-player/static/js/runtime-main.93dabac1.js"
+  }
+]);
